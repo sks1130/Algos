ea4a0221431777bebc4a89733823093ed2807ed7
sachin <sachin.srivastava@cointribe.com>
2017-08-19 03:40:21 +0530

    commit.

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -1,11 +1,7 @@
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.NumberFormat;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -27,8 +23,6 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javassist.bytecode.stackmap.TypeData.ClassName;
 
 /**
  * @author sachin.srivastava
@@ -386,8 +380,10 @@ public class HackerRank {
 	public static final Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int num = 8;
-		System.out.println(num + " is " + new HackerRank.Inner().new Private().powerof2(num));
+		int[] arr = new int[]{3,4,5,1,3,2,5,7,12,1,22,2,3,4,3};
+		//quickSort(arr);
+		mergeSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 	static class Inner {
@@ -427,7 +423,73 @@ public class HackerRank {
 		}
 		return hexStr;
 	}
+	public static void quickSort(int[] arr){
+		quickSort(arr, 0, arr.length -1);
+	}
+	
+	public static void mergeSort(int[] arr){
+		mergeSort(arr, new int[arr.length] ,0, arr.length-1);
+	}
+	public static void mergeSort(int[] arr, int[] temp, int leftStart , int rightEnd){
+		if(leftStart >= rightEnd){
+			return;
+		}
+		int mid = (leftStart+rightEnd)/2;
+		mergeSort(arr,temp, leftStart, mid);
+		mergeSort(arr,temp, mid+1, rightEnd);
+		merge(arr,temp, leftStart , rightEnd);
+	}
+	
+	public static void merge(int[] arr , int[] temp, int leftStart , int rightEnd){
+		int leftEnd = (leftStart+rightEnd)/2;
+		int rightStart = leftEnd +1;
+		int size = rightEnd - leftStart +1;
+		
+		int left = leftStart;
+		int right = rightStart;
+		int index = leftStart;
+		
+		while(left<= leftEnd && right <= rightEnd){
+			if(arr[left] <= arr[right]){
+				temp[index] = arr[left];
+				left++;
+			} else {
+				temp[index] = arr[right];
+				right++;
+			}
+			index++;
+		}
+		System.arraycopy(arr, left, temp, index, leftEnd-left+1);
+		System.arraycopy(arr, right, temp, index, rightEnd-right+1);
+		System.arraycopy(temp, leftStart, arr, leftStart, size);
+	}
 
+	public static void quickSort(int[] arr, int left, int right) {
+		if (left >= right) {
+			return;
+		}
+		int pivot = arr[(left + right) / 2];
+		int index = partition(arr, left, right, pivot);
+		quickSort(arr, left, index - 1);
+		quickSort(arr, index, right);
+	}
+
+	public static int partition(int[] arr, int left, int right, int pivot) {
+		while (left <= right) {
+			while (arr[left] < pivot) {
+				left++;
+			}
+			while (arr[right] > pivot) {
+				right--;
+			}
+			if (left <= right) {
+				swap(arr, left, right);
+				left++;
+				right--;
+			}
+		}
+		return left;
+	}
 	static class MyCalculator {
 		/*
 		 * Create the method long power(int, int) here.
